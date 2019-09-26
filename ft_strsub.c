167b71458f05b9cf5ee2bc73a0580df3e8b83a0f
romik1505 <romik1505@yandex.ru>
2019-09-26 11:28:06 +0300

    strsub fix -Werror

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: cniranye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 10:58:48 by cniranye          #+#    #+#             */
-/*   Updated: 2019/09/24 20:48:49 by cniranye         ###   ########.fr       */
+/*   Updated: 2019/09/26 11:27:25 by cniranye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*tmp;
-	size_t	i;
 
-	i = 0;
 	if (!s || (len + 1 == 0))
 		return (0);
 	if (!(tmp = ft_strnew(len)))
